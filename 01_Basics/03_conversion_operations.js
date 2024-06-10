74134628398711b4fc5a98fdb81e66ec456bb6b5
ashishshres <ashishshr27@gmail.com>
2024-06-10 16:30:19 +0545

    conversion operations in js

---
+++ 01_Basics/03_conversion_operations.js
@@ -0,0 +1,104 @@
+// let score = 3;
+// console.log(typeof score); // number
+
+// let score = "3";
+// console.log(typeof score); // string
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+
+// let score = "33a";
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN
+
+// let score = null;
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // 0
+
+// let score = undefined;
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN
+
+// let score = true;
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // 1
+
+// let score = "Ashish";
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // number
+// console.log(valueInNumber); // NaN
+
+// "3" => 3
+// "3abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn); // boolean
+// console.log(booleanIsLoggedIn); // 1
+
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn); // boolean
+// console.log(booleanIsLoggedIn); // false
+
+// let isLoggedIn = "Ashish";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn); // boolean
+// console.log(booleanIsLoggedIn); // true
+
+// 1 => true; 0 => false
+// "" => false
+// "Ashish" => true
+
+// let someNumber = 8;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber); // 8
+// console.log(typeof stringNumber); // string
+
+// operations
+let value = 3;
+let negValue = -value;
+// console.log(negValue); // -3
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+// let str1 = "hello";
+// let str2 = " ashish";
+
+// let str3 = str1 + str2;
+// console.log(str3); // hello ashish
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122 => string first
+// console.log(1 + 2 + "2"); // 32 => number first
+// console.log(1 + "2" + 3); // 123
+// console.log(typeof (1 + "2" + 3)); // string
+
+// console.log(+true);
+// console.log(true+); // error
+
+// console.log(+""); // 0
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2;
+
+// let scoreCounter = 100;
+// scoreCounter++;
+// console.log(scoreCounter);
+
+// let scoreCounter = 100;
+// ++scoreCounter;
+// console.log(scoreCounter);

992958af6186f214d696774222c80e24d4270d44
ashishshres <ashishshr27@gmail.com>
2024-06-10 16:30:35 +0545

    strings in js

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,54 @@
+const name = "Ashish";
+const repoCount = 20;
+
+// console.log("Hello, my name is " + name + " and my repo count is " + repoCount); // not recommended
+
+// string interpolation
+// console.log(`Hello, ny name is ${name} and my repo count is ${repoCount}`);
+
+// const movieName = new String("Spider Man");
+
+// console.log(movieName);
+
+// console.log(movieName[0]);
+// console.log(movieName.__proto__);
+
+// console.log(movieName.length);
+// console.log(movieName.toUpperCase());
+// console.log(movieName.toLowerCase());
+// console.log(movieName.charAt(3));
+// console.log(movieName.indexOf("d"));
+
+const movieName = new String("Spider-Man");
+// ider-Man
+// Spide
+// Spider-M
+// ide
+// const newString = movieName.substring(0, 4);
+
+// console.log(newString);
+
+// const anotherString = movieName.slice(-5, -1);
+// const anotherString = movieName.slice(3, 3);
+// const anotherString = movieName.slice(4, 3);
+// const anotherString = movieName.slice(2, 5);
+// const anotherString = movieName.slice(-8, 5);
+const anotherString = movieName.slice(5, 8);
+
+console.log(anotherString);
+
+// const newString = "   user     ";
+// console.log(newString);
+// console.log(newString.trim());
+
+// const url = "https://example.com/example%20about";
+
+// console.log(url.replace("%20", "-"));
+
+// console.log(url.includes("about"));
+
+// console.log(url.includes("home"));
+
+// const superHero = "spider-man";
+
+// console.log(superHero.split("-"));

16e9c2b8a3149cb21f42652f53476e7a93ca92a6
ashishshres <ashishshr27@gmail.com>
2024-06-10 16:30:43 +0545

    numbers and math in js

---
+++ 01_Basics/06_numbers_and_math.js
@@ -0,0 +1,36 @@
+// Numbers
+
+// const balance = 200;
+// const balance = Number(200);
+// const balance = new Number(200);
+
+// console.log(typeof balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 3.1478943;
+// const otherNumber = 123.89;
+// const otherNumber = 1123.89;
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// Math
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(3.6));
+// console.log(Math.ceil(3.2));
+// console.log(Math.floor(3.9));
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+// const min = 10;
+// const max = 20;
+
+// (Math.floor(Math.random() * (max - min + 1)) + min);
+// console.log(M/ath.floor(Math.random() * (max - min + 1)) + min);

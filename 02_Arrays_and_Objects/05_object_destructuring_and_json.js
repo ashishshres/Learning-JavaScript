1f70175f92ca4497bbf1fcbad63c37742f53d429
ashishshres <ashishshr27@gmail.com>
2024-06-13 16:28:59 +0545

    object destructuring in js

---
+++ 02_Arrays_and_Objects/05_object_destructuring_and_json.js
@@ -0,0 +1,21 @@
+const course = {
+  name: "JavaScript",
+  price: "999",
+  instructor: "Hitesh Choudhary",
+};
+
+// course.name
+
+// const { instructor } = course;
+const { instructor: tutor } = course;
+
+// console.log(instructor);
+console.log(tutor);
+
+//JSON
+
+// {
+//   "name": "Ashish Shrestha",
+//   "course": "JavaScript",
+//   "price": "free"
+// }

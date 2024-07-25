a872b73a8712828acc439c41bcc0ff8b55c7c2ef
ashishshres <ashishshr27@gmail.com>
2024-07-25 19:23:19 +0545

    variables in js

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,19 +1,19 @@
-const accountId = 54321
-let accountEmail = "ashish@example.com"
-var accountPassword = "12345"
-accountCity = "Hetauda" // not good practice
-let accountState // undefined by default if declared only
+const accountId = 54321;
+let accountEmail = "ashish@example.com";
+var accountPassword = "12345";
+accountCity = "Hetauda"; // not good practice
+let accountState; // undefined by default if declared only
 
 // accountId = 12345 // not allowed because of constant type
 
-accountEmail = "as@shr.com"
-accountPassword = "123456789"
-accountCity = "Chitwan"
+accountEmail = "as@shr.com";
+accountPassword = "123456789";
+accountCity = "Chitwan";
 
 /*
 Prefer not to use var, 
 because of the issue in block scope and functional scope
 */
 
-console.log(accountId)
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);

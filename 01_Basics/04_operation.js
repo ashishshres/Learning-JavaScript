31f8bb1d87e92a9b6cb8c97a4241b0ec1c3d7f29
ashishshres <ashishshr27@gmail.com>
2024-07-25 21:51:50 +0545

    operations in js

---
+++ 01_Basics/04_operation.js
@@ -0,0 +1,42 @@
+let value = 3;
+let negValue = -value;
+// console.log(value);
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 / 2);
+// console.log(2 ** 2);
+// console.log(2 % 2);
+
+let str1 = "hello";
+let str2 = " ashish";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+// console.log(true); // true
+// console.log(+true); // 1
+// console.log(true+); // invalid
+// console.log(+""); // 0
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 1 + 1; // should be readability
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+let gameCounter = 100;
+
+// gameCounter++;
+// console.log(gameCounter); // 101
+console.log(gameCounter++); // 100
+
+// ++gameCounter;
+// console.log(gameCounter); // 101
+console.log(++gameCounter); // 102
+

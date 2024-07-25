6193479eaf820a7dc147d9e661903bdc39852e02
ashishshres <ashishshr27@gmail.com>
2024-07-25 19:15:54 +0545

    datatypes in js

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JavaScript code as newer version
+
+// alert(3 + 3) // we are using nodejs, not a browser
+
+// code readability should be high
+// console.log(3
+//     +3
+// )
+
+// console.log("Ashish")
+
+let name = "Ashish" // string
+let age = 18 // number
+let isLoggedIn = false // boolean
+
+// primitive datatypes
+// number
+// bigint
+// string => "" or ''
+// boolean => true or false
+// null => standalone value in JavaScript
+// undefined => value not defined
+// symbol => unique
+
+// non-primitive datatypes
+// object
+// array
+
+console.log(typeof 10) // number
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined

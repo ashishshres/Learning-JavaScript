24d110b807d482aff347d62bc7a426a0e7e4e41d
ashishshres <ashishshr27@gmail.com>
2024-07-26 20:47:13 +0545

    operators in js

---
+++ Exercises/02_operators.js
@@ -0,0 +1,57 @@
+// Task 1: Write a program to add two numbers and log the result to the console.
+console.log(2 + 2); // 4
+
+// Task 2: Write a program to subtract two numbers and log the result to the console.
+console.log(4 - 2); // 2
+
+// Task 3: Write a program to multiply two numbers and log the result to the console.
+console.log(4 * 2); // 8
+
+// Task 4: Write a program to divide two numbers and log the result to the console.
+console.log(4 / 2); // 2
+
+// Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+console.log(4 % 2); // 0
+
+// Task 6: Use the `+=` operator to add a number to a variable and log the result to the console.
+let num1 = 10;
+num1 += 20;
+console.log(num1); // 30
+
+// Task 7: Use the `-=` operator to subtract a number from a variable and log the result to the console.
+let num2 = 30;
+num2 -= 10;
+console.log(num2); // 20
+
+// Task 8: Write a program to compare two numbers using `>` and `<` and log the result to the console.
+console.log(10 > 20); // false
+console.log(10 < 20); // true
+
+// Task 9: Write a program to compare two numbers using `>=` and `<=` and log the result to the console.
+console.log(10 >= 10); // true
+console.log(10 <= 20); // true
+
+// Task 10: Write a program to compare two numbers using `==` and `===` and log the result to the console.
+console.log("10" == 10); // true
+console.log("10" === 10); // false
+
+// Task 11: Write a program that uses the `&&` operator to combine two conditions and log the result to the console.
+console.log(true && true); // true
+console.log(true && false); // false
+
+// Task 12: Write a program that uses the `||` operator to combine two conditions and log the result to the console.
+console.log(true || true); // true
+console.log(true || false); // true
+
+// Task 13: Write a program that uses the `!` operator to combine two conditions and log the result to the console.
+console.log(!true); // false
+console.log(!false); // true
+
+// Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+let numOne = 10;
+let numTwo = -10;
+
+(numOne < 0)?console.log("Negative Number"): console.log("Positive Number"); // Positive Number
+(numTwo < 0)?console.log("Negative Number"): console.log("Positive Number"); // Negative Number
+
+

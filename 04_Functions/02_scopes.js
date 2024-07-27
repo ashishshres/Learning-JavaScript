e76be3d6bdc24f5e1464d7f222581fb1db864f9c
ashishshres <ashishshr27@gmail.com>
2024-07-27 09:15:40 +0545

    scopes in js

--- 04_Functions/02_scopes.js
+++ 04_Functions/02_scopes.js
@@ -29,13 +29,58 @@
 
 // console.log(c); // 30
 
-let c = 100;
+// let c = 100;
 
-if (true) {
-    let a = 10;
-    const b = 20;
-    let c = 30;
-    console.log("INNER: ", c); // 30
+// if (true) {
+//     let a = 10;
+//     const b = 20;
+//     let c = 30;
+//     console.log("INNER: ", c); // 30
+// }
+
+// console.log("OUTER:", c); // 100
+
+function one() {
+    const username = "Ashish";
+
+    function two() {
+        const website = "youtube";
+        console.log(username); // Ashish
+    }
+    // console.log(website); // not defined
+
+    two();
 }
 
-console.log("OUTER:", c); // 100
+// one();
+
+if (true) {
+    const username = "Ashish";
+    if (username === "Ashish") {
+        const website = " youtube";
+        // console.log(username + website); // Ashish youtube
+    }
+    // console.log(website); // not defined
+}
+
+// console.log(username); // not defined
+
+// ++++++++++ interesting ++++++++++
+
+// console.log(addone(5)); // 6
+
+function addone(num) {
+    return num + 1;
+}
+
+// console.log(addone(5)); // 6
+
+// function expression
+
+// console.log(addTwo(5)); // cannot access before initialization
+
+const addTwo = function (num) {
+    return num + 1;
+};
+
+console.log(addTwo(5)); // 6

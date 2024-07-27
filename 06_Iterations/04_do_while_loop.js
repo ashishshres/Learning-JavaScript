6cc24aa507a4b07e75ddcf2d4d0ddb1f04ff374c
ashishshres <ashishshr27@gmail.com>
2024-07-27 16:55:38 +0545

    do while loops in js

---
+++ 06_Iterations/04_do_while_loop.js
@@ -0,0 +1,15 @@
+// do {
+
+// } while (condition);
+
+// let score = 1;
+// do {
+//     console.log(`Score is ${score}`);
+//     score++;
+// } while (score <= 10);
+
+let score = 11;
+do {
+    console.log(`Score is ${score}`); // Score is 11
+    score++;
+} while (score <= 10);

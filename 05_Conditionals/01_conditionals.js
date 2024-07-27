396710c0c966dd0364e452192c2a8684b82e6f59
ashishshres <ashishshr27@gmail.com>
2024-07-27 15:46:08 +0545

    conditionals in js

--- 05_Conditionals/01_conditionals.js
+++ 05_Conditionals/01_conditionals.js
@@ -60,3 +60,31 @@ if (score > 100) {
 }
 // console.log(`User power: ${power}`); // power is not defined
 console.log(`User power: ${power}`); // var used
+
+// const balance = 1200;
+// if (balance > 500) console.log("test");
+// if (balance > 500) console.log("test1"), console.log("test2"); // not recommended
+
+// const balance = 1000;
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+
+if (userLoggedIn && debitCard) {
+    console.log("allow to buy courses");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}

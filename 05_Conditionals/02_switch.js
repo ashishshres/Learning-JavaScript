e16bd8afe15008bd3fbf57c6076e5e7e88251006
ashishshres <ashishshr27@gmail.com>
2024-07-27 16:15:00 +0545

    switch in js

---
+++ 05_Conditionals/02_switch.js
@@ -0,0 +1,32 @@
+// const month = 3; // March
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     default:
+//         break;
+// }
+
+const month = "feb"; // March
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    default:
+        break;
+}

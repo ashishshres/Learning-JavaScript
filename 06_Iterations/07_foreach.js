f7a86dcd3a34f7d1a5640d208ec4924a2e4e7fb0
ashishshres <ashishshr27@gmail.com>
2024-07-27 17:38:48 +0545

    foreach loops in js

---
+++ 06_Iterations/07_foreach.js
@@ -0,0 +1,44 @@
+// forEach
+
+// array.forEach(element => {
+
+// });
+
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// coding.forEach(function (item) {
+//     console.log(item); // js ruby java python cpp
+// });
+
+// coding.forEach((item) => {
+//     console.log(item);
+// });
+
+// function printMe(item) {
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+// });
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageExtension: "js",
+    },
+    {
+        languageName: "java",
+        languageExtension: "java",
+    },
+    {
+        languageName: "python",
+        languageExtension: "py",
+    },
+];
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+});

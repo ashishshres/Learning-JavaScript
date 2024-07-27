08dab2172c926fc513ee12aa57fc3dab82a51560
ashishshres <ashishshr27@gmail.com>
2024-07-27 16:43:40 +0545

    break and continue in js

---
+++ 06_Iterations/02_break_and_continue.js
@@ -0,0 +1,19 @@
+// break and continue
+
+// break
+for (let i = 1; i <= 20; i++) {
+    if (i === 5) {
+        console.log("Detected 5");
+        break;
+    }
+    console.log(`Value of i is ${i}`);
+}
+
+// continue
+for (let i = 1; i <= 20; i++) {
+    if (i === 5) {
+        console.log("Detected 5");
+        continue;
+    }
+    console.log(`Value of i is ${i}`);
+}

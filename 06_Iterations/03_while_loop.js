bb84b3f1e2c31b6057ba0b4dff557228e6a79bf0
ashishshres <ashishshr27@gmail.com>
2024-07-27 16:55:27 +0545

    while loops in js

---
+++ 06_Iterations/03_while_loop.js
@@ -0,0 +1,16 @@
+// while (condition) {
+
+// }
+
+// let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+// }
+
+let fruits = ["apple", "banana", "cherry"];
+let i = 0;
+while (i < fruits.length) {
+    console.log(`Value is ${fruits[i]}`);
+    i++;
+}
